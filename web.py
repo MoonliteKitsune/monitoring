0158f247bad7661c908ae91981b79c2af555ce2c
MoonliteKitsune <nathanbartier22022006@gmail.com>
2025-04-04 15:02:09 +0200

    Update web.py

--- web.py
+++ web.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 @app.route('/')
-def accueil():
-    return "<h1>Bienvenue sur mon serveur Flask dans la VM ! 🎉</h1><p>Ça fonctionne !</p>"
+def ouvrir_fichier_html():
+    # Renvoie le fichier 'resultat.html' situé dans le même dossier que web.py
+    return send_file('graphiques_sondes.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
